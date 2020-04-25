be6a16833678c07f0217fd0dfed318b7f901f86f
Khalid <kabcoder@gmail.com>
2020-04-25 15:28:37 +0100

    finished with the javascript section

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
-var hello;
+function init(){
+    let toogleIn = document.querySelector('.toogle--in');
+    let toogleOut = document.querySelector('.toogle-out');
+    let body = document.querySelector('body');
+    let popUp = document.querySelector('.pop--up');
+    let links = document.querySelectorAll('.nav--container li');
+
+    function animationStart(){
+        popUp.style.transform = 'translate(scale(1))';
+        popUp.style.visibility = 'visible';
+        popUp.style.opacity = '1';
+      setTimeout(function(){
+        popUp.style.transform = 'translate(scale(0))';
+      }, 1000)
+    }
+
+    function animationEnd(){
+        popUp.style.opacity = '0';
+        popUp.style.visibility = 'hidden';
+    }
+    let linksArr = Array.from(links)
+
+    linksArr.forEach(function(arr){
+        arr.addEventListener('click', animationEnd);
+    })
+    console.log(linksArr)
+
+
+
+    toogleIn.addEventListener('click', animationStart);
+    toogleOut.addEventListener('click', animationEnd);
+}
+
+init();
 
