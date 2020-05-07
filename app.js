f113e89bf48e000d6b754d81ed20431834a945d1
Khalid <kabcoder@gmail.com>
2020-05-07 08:37:26 +0100

    improving UX

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-function init(){
-    let toogleIn = document.querySelector('.toogle--in');
-    let toogleOut = document.querySelector('.toogle-out');
+window.addEventListener('load', (e) =>{
+
+  setTimeout(()=>{
+    const displayMain = ()=>{
+
+      let main = document.querySelector('.main-web');
+      let loader = document.querySelector('.page--loader');
+      main.style.display = "block";
+      loader.style.opacity = '0';
+      loader.style.display = 'none';
+
+      const init = ()=>{
+        let toogleIn = document.querySelector('.toogle--in');
+       let toogleOut = document.querySelector('.toogle-out');
     let body = document.querySelector('body');
     let popUp = document.querySelector('.pop--up');
     let links = document.querySelectorAll('.nav--container li');
 
     function animationStart(){
-        popUp.style.transform = 'translate(scale(0))';
-        popUp.style.visibility = 'visible';
-        popUp.style.opacity = '0';
-      setTimeout(function(){
         popUp.style.transform = 'translate(scale(1))';
+        popUp.style.visibility = 'visible';
         popUp.style.opacity = '1';
-      }, 500)
     }
 
     function animationEnd(){
@@ -26,7 +33,16 @@ function init(){
     })
     toogleIn.addEventListener('click', animationStart);
     toogleOut.addEventListener('click', animationEnd);
-}
 
-init();
+      }
+
+      init();
+
+   }
+
+    displayMain();
+  }, 2000)
+
+
+})
 
